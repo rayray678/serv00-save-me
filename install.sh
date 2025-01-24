ed7b9ac9d30f3c79799ca861195f66ea93789696
人皇帝辛 <120008435@qq.com>
2025-01-25 01:21:51 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 print_status() {
     local message=$1
     local success=$2
+
+    # 动画字符定义
     animation=("▖" "▘" "▝" "▗")
     for i in {1..8}; do
         printf "\r[%s] %s" "${animation[$((i % 4))]}" "$message"
@@ -9,11 +12,10 @@ print_status() {
     done
 
     if [[ $success -eq 0 ]]; then
-        printf "\r[\033[0;32mOK\033[0m] %s\n" "$message" # 绿色OK
+        printf "\r[\033[0;32mOK\033[0m] %s\n" "$message" # 成功绿色OK
     else
-        printf "\r[\033[0;31mNO\033[0m] %s\n" "$message" # 红色NO
+        printf "\r[\033[0;31mNO\033[0m] %s\n" "$message" # 失败红色NO
     fi
-    sleep 3
 }
 
 U1=$(whoami)
@@ -25,41 +27,43 @@ L1="https://raw.githubusercontent.com/ryty1/sver00-save-me/refs/heads/main/app.j
 
 echo ""
 echo " ———————————————————————————————————————————————————————————— "
-print_status " 正在删除 默认域名 " 2
+
+# 删除默认域名
+print_status "正在删除 默认域名" 2
 cd && devil www del "$D1" > /dev/null 2>&1 && [[ -d "$D2" ]] && rm -rf "$D2" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    print_status " 默认域名 删除成功 " 0
+    print_status "默认域名 删除成功" 0
 else
-    print_status " 默认域名 删除失败 或 不存在 " 1
+    print_status "默认域名 删除失败 或 不存在" 1
 fi
 
 # 创建类型域名
-print_status " 正在创建 类型域名 " 2
+print_status "正在创建 类型域名" 2
 devil www add "$D1" nodejs /usr/local/bin/node22 > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    print_status " 类型域名 创建成功 " 0
+    print_status "类型域名 创建成功" 0
 else
-    print_status " 类型域名 创建失败，请检查环境设置 " 1
+    print_status "类型域名 创建失败，请检查环境设置" 1
     exit 1
 fi
 
 # 安装环境依赖
-print_status " 正在安装 环境依赖 " 2
+print_status "正在安装 环境依赖" 2
 cd "$D2" && npm init -y > /dev/null 2>&1 && npm install dotenv basic-auth express > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    print_status " 环境依赖 安装成功 " 0
+    print_status "环境依赖 安装成功" 0
 else
-    print_status " 环境依赖 安装失败 " 1
+    print_status "环境依赖 安装失败" 1
     exit 1
 fi
 
 # 下载配置文件
-print_status " 正在下载 配置文件 " 2
+print_status "正在下载 配置文件" 2
 curl -s -o "$F1" "$L1" && chmod 755 "$F1" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
-    print_status " 配置文件 下载成功 " 0
+    print_status "配置文件 下载成功" 0
 else
-    print_status " 配置文件 下载失败 " 1
+    print_status "配置文件 下载失败" 1
     exit 1
 fi
 
