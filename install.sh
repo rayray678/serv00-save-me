e85a8c6ad724edb4b6b2ce8e93a904300c8a1ee2
人皇帝辛 <120008435@qq.com>
2025-01-25 02:32:11 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,19 @@ print_status() {
     local success=$2
     local start_time=$(date +%s)
     local min_time=2
-    local animation=("/" "-" "\\" "|")
+    local animation=("-" "\\" "|" "/")
 
     while true; do
         local elapsed_time=$(( $(date +%s) - start_time ))
         printf "\r[%s] %s" "${animation[$((elapsed_time % 4))]}" "$message"
+        
         if [[ $elapsed_time -ge $min_time ]]; then
             break
         fi
         sleep 0.15
     done
     printf "\r                       \r"
+    
     if [[ $success -eq 0 ]]; then
         printf "[\033[0;32mOK\033[0m] %s\n" "$message"
     else
@@ -28,20 +30,16 @@ D1="$U1_DOMAIN.serv00.net"
 D2="/home/$U1/domains/$D1"
 F1="$D2/public_nodejs/app.js"
 L1="https://raw.githubusercontent.com/ryty1/sver00-save-me/refs/heads/main/app.js"
-
 echo ""
 echo " ———————————————————————————————————————————————————————————— "
-
-# 删除默认域名
 cd && devil www del "$D1" > /dev/null 2>&1 && [[ -d "$D2" ]] && rm -rf "$D2" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     print_status "正在删除 默认域名" 0
 else
     print_status "默认域名 删除失败 或 不存在" 1
 fi
-sleep 1.5  # 每个命令间休息 1.5 秒
+sleep 1.5
 
-# 创建类型域名
 devil www add "$D1" nodejs /usr/local/bin/node22 > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     print_status "正在创建 类型域名" 0
@@ -51,7 +49,6 @@ else
 fi
 sleep 1.5
 
-# 安装环境依赖
 cd "$D2" && npm init -y > /dev/null 2>&1 && npm install dotenv basic-auth express > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     print_status "正在安装 环境依赖" 0
@@ -61,7 +58,6 @@ else
 fi
 sleep 1.5
 
-# 下载配置文件
 curl -s -o "$F1" "$L1" && chmod 755 "$F1" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     print_status "正在下载 配置文件" 0
