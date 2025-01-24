8902d055969bb952a657aaf7a3bbd87b6b7e9892
人皇帝辛 <120008435@qq.com>
2025-01-25 00:40:57 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -15,20 +15,43 @@ print_status() {
         printf "\r[\033[0;31mNO\033[0m] %s\n" "$message" # 红色NO
     fi
 }
+
 U1=$(whoami)
 U1_DOMAIN=$(echo "$U1" | tr '[:upper:]' '[:lower:]')
 D1="$U1_DOMAIN.serv00.net"
 D2="/home/$U1/domains/$D1"
 F1="$D2/public_nodejs/app.js"
 L1="https://raw.githubusercontent.com/ryty1/sver00-save-me/refs/heads/main/app.js"
+
 echo ""
 echo " ———————————————————————————————————————————————————————————— "
-cd && devil www del "$D1" > /dev/null 2>&1 || print_status " 默认域名 删除失败 或 不存在 " && print_status " 默认域名 删除成功 "
-[[ -d "$D2" ]] && rm -rf "$D2"
-devil www add "$D1" nodejs /usr/local/bin/node22 > /dev/null 2>&1 || { print_status " 类型域名 创建失败，请检查环境设置 "; exit 1; } && print_status " 类型域名 创建成功 "
-[[ ! -d "$D2" ]] && mkdir -p "$D2"
-cd "$D2" && npm init -y > /dev/null 2>&1 && npm install dotenv basic-auth express > /dev/null 2>&1 || { print_status " 环境依赖 安装失败 "; exit 1; } && print_status " 环境依赖 安装成功 "
-curl -s -o "$F1" "$L1" && chmod 755 "$F1" && print_status " 配置文件 下载成功 " || { print_status " 配置文件 下载失败 "; exit 1; }
+cd && devil www del "$D1" > /dev/null 2>&1  && [[ -d "$D2" ]] && rm -rf "$D2" > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    print_status "默认域名 删除成功" 0
+else
+    print_status "默认域名 删除失败 或 不存在" 1
+fi
+devil www add "$D1" nodejs /usr/local/bin/node22 > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    print_status "类型域名 创建成功" 0
+else
+    print_status "类型域名 创建失败，请检查环境设置" 1
+    exit 1
+fi
+cd "$D2" && npm init -y > /dev/null 2>&1 && npm install dotenv basic-auth express > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    print_status "环境依赖 安装成功" 0
+else
+    print_status "环境依赖 安装失败" 1
+    exit 1
+fi
+curl -s -o "$F1" "$L1" && chmod 755 "$F1" > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    print_status "配置文件 下载成功" 0
+else
+    print_status "配置文件 下载失败" 1
+    exit 1
+fi
 echo " ———————————————————————————————————————————————————————————— "
 echo ""
 echo " 【 恭 喜 】： 网 页 保 活 一 键 部 署 已 完 成  "
@@ -39,4 +62,4 @@ echo " |**查看节点 https://$D1/node_info "
 echo ""
 echo " |**输出日志 https://$D1/keepalive "
 echo " ———————————————————————————————————————————————————————————— "
-echo "" 
+echo ""
